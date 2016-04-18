e8f00ebefc3f15ebf8c50ee7960b4bb78d0e0136
Made In Moon <madeinmoon.agency@gmail.com>
2016-04-18 14:12:41 +0200

    Summer Snow

--- server/config/authentification.js
+++ server/config/authentification.js
@@ -6,8 +6,8 @@ export const setGoogleAuthConfig = () =>  {
     { service: "google" },
     {
       $set: {
-        clientId: "596443200401-gbfcu4m338a7jr9me3lp57tr4lra1dj6.apps.googleusercontent.com",
-        secret:   "k-sdC14cjRoH8aQhn3nVJrUO"
+        clientId: "",
+        secret:   ""
       }
     }
   );
@@ -20,8 +20,8 @@ export const setFacebookAuthConfig = () =>  {
     { service: "facebook" },
     {
       $set: {
-        appId:  "527109410805279",
-        secret: "5edadf9f160338d928444a6b5cac0993"
+        appId:  "",
+        secret: ""
       }
     }
   );
